8ce927d5e5787f7367d1ea8d52c516b15d80be97
Ilya <Ilya@DESKTOP-CI3R4V0>
2025-03-22 16:15:11 +0300

    Добавьте файлы проекта.

---
+++ Test/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Operations
+    {
+
+    }
+
+    public double addition()
+    {
+        return 1;
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("op: ");
+var imput = Console.ReadLine();
+
+if (imput == " ")
+{
+    Console.Write("args: ");
+    Console.ReadLine();
+}
+else Console.WriteLine("Incorrect input");
